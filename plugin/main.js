256142ad4acf9928851bf4372320b7b7fcbc61f5
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-15 23:14:29 +0530

    Update main.js

--- plugin/main.js
+++ plugin/main.js
@@ -4,12 +4,15 @@ const path = require('path');
 const fs = require('fs');
 
 
-router.get('/login_or_sign', async (req, res, next) => {
+router.get('/ack', async (req, res, next) => {
     if(!req.query.code) {
     res.sendFile(__path + '/plugin/login.html')
     } else {
         await fs.writeFileSync(__path + '/plugin/external.html','<html><body>hy</body></html>');
-        res.sendFile(__path + '/plugin/external.html')
+        res.redirect('/plugins/list');
     }
 })
+router.get('/list', async (req, res, next) => {
+    res.sendFile(__path + '/plugin/external.html')
+});
 module.exports = router
