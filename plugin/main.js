88ea33e4bf030c8addf2d6247bc4c635ac54ae2a
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-15 23:00:19 +0530

    Update main.js

--- plugin/main.js
+++ plugin/main.js
@@ -1,12 +1,15 @@
 let express = require('express');
 let router = express.Router();
 const path = require('path');
+const fs = require('fs');
+
 
 router.get('/login_or_sign', async (req, res, next) => {
     if(!req.query.code) {
     res.sendFile(__path + '/plugin/login.html')
     } else {
-    res.json({status:"working on it"});
+        await fs.writeFileSync(__path + '/plugin/external.html','<html><body>hy</body></html>');
+        res.sendFile(__path + '/plugin/external.html')
     }
 })
 module.exports = router
