1b0c96eb30011d24095d53a33d377508c5504b91
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-16 06:46:41 +0530

    Update main.js

--- plugin/main.js
+++ plugin/main.js
@@ -1,5 +1,5 @@
-let express = require('express');
-let router = express.Router();
+const express = require('express');
+const router = express.Router();
 const path = require('path');
 const fs = require('fs');
 
@@ -9,16 +9,12 @@ router.get('/ack', async (req, res, next) => {
     res.sendFile(__path + '/plugin/login.html')
     } else {
         await fs.writeFileSync(__path + '/plugin/external.html','<html><body>hy</body></html>');
+        await new Promise(resolve => setTimeout(resolve, 200));
         return await res.redirect('/plugins/list');
     }
 })
 router.get('/list', async (req, res, next) => {
-    if(!fs.existsSync(__path + '/plugin/external.html')) {
-        return res.render('/plugins/emdi', function (err, html) {
-            res.send(html)
-        })
-        res.redirect('/plugin/ack');
-    }   
+    if(!fs.existsSync(__path + '/plugin/external.html')) res.redirect('/plugins/ack');
     res.sendFile(__path + '/plugin/external.html')
 });
 module.exports = router
