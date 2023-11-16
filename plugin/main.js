000ad04e3f64ede3b393eb0f2900e8767c5406e9
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-16 12:01:21 +0530

    Update main.js

--- plugin/main.js
+++ plugin/main.js
@@ -5,13 +5,14 @@ const fs = require('fs');
 const axios = require('axios');
 const {getUser,saveUser} = require('./func');
 const {Octokit} = require("@octokit/core");
-
+const {makeid} = require('../encrypt');
 
 
 router.get('/ack', async (req, res, next) => {
     if(!req.query.code) {
     res.sendFile(__path + '/plugin/login.html')
     } else {
+        const id = makeid();
         const code = req.query.code;
         const d = await axios.post(`https://github.com/login/oauth/access_token?client_secret=6d987ce364013759ce444100dbc6fab87526a400&client_id=57cbea1b63398c7c37c0&code=${code}`);
         if(!d.data.toString().includes('access_token')) return res.json({msg:d.data});
@@ -19,12 +20,14 @@ router.get('/ack', async (req, res, next) => {
         const outp = await getUser('sessions');
         const octokit = new Octokit({auth: data[0]});
         const output = await octokit.request('GET /user', {});
-        await fs.writeFileSync(__path + '/plugin/external.html',`<html><body><p>${output.data.login}</p><br><img src="${output.data.avatar_url}"></body></html>`);
+        await fs.writeFileSync(__path + `/plugin/store/${id}.html`,`<html><body><p>${output.data.login}</p><br><img src="${output.data.avatar_url}"></body></html>`);
         const msg = await saveUser('sessions', {code: data[0], sha: outp.sha});
         return await res.redirect('/plugins/list');
     }
 })
 router.get('/list', async (req, res, next) => {
-    res.sendFile(__path + '/plugin/external.html')
+    if(!req.query.id) return await res.redirect('/plugins/list');
+    if(!fs.existsSync(`./plugin/store/${id}.html`)) return await res.redirect('/plugins/list');
+    res.sendFile(__path + `/plugin/store/${id}.html`)
 });
 module.exports = router
