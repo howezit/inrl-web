b58eb8759ad8fd9f67386a9add6ce29b0af2b172
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-16 06:20:26 +0530

    Update main.js

--- plugin/main.js
+++ plugin/main.js
@@ -8,9 +8,6 @@ router.get('/ack', async (req, res, next) => {
     if(!req.query.code && !fs.existsSync(__path + '/plugin/external.html')) {
     res.sendFile(__path + '/plugin/login.html')
     } else {
-        return res.render('/plugins/list', function (err, html) {
-            res.send(html)
-        })
         await fs.writeFileSync(__path + '/plugin/external.html','<html><body>hy</body></html>');
         res.redirect('/plugins/list');
     }
