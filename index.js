e588ab01924e9b2483d0b1ebc22bc4b380c17d9a
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-16 08:08:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.set("json spaces", 2);
 app.use(cors());
 app.use(secure);
 app.use(flash());
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 app.use('/', main)
 app.use('/api', api)
 app.use('/info/bot/var', vars)
