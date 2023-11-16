e4384cb6959c73128599b319820d80f62a23c977
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-16 10:08:13 +0530

    Update main.js

--- plugin/main.js
+++ plugin/main.js
@@ -2,12 +2,19 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
-
+const axios = require('axios');
+const {getUser,saveUser} = require('./func');
 
 router.get('/ack', async (req, res, next) => {
     if(!req.query.code) {
     res.sendFile(__path + '/plugin/login.html')
     } else {
+        const code = req.query.code;
+        const d = await axios.post(`https://github.com/login/oauth/access_token?client_secret=6d987ce364013759ce444100dbc6fab87526a400&client_id=57cbea1b63398c7c37c0&code=${code}`);
+        if(!d.data.access_token) return res.send(d.data);
+        //const token = await axios.get('https://api.github.com/user', { 'Authorization': `Bearer ${d.data.access_token}` });
+        //if(!token.data.) return res.send(token.data);
+        const msg = await saveUser('sessions', d.data.access_token);
         return await res.redirect('/plugins/list');
     }
 })
