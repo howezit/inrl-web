211b9b4d605776441afbe4dcffaa66be9a3c0103
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-18 18:50:34 +0530

    Update make.js

--- plugin/make.js
+++ plugin/make.js
@@ -3,7 +3,6 @@ return `<html>
   <head></head>
   <title>Age Calculator</title>
   <script crossorigin="anonymous" src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.0.0-alpha.1/axios.min.js"></script>
-  <script type="module" crossorigin src="/func.js"></script>
   <link rel="stylesheet" href="/style.css">
   <body>
     <p>${output.data.login}</p>
@@ -12,6 +11,14 @@ return `<html>
     <br>
     <br>${json}<br>
     <p id="like">clicked</p>
+    <script>
+const a = document.getElementById("like");
+async function u(p, n) {
+      alert(p+",,, "+n);
+      await axios(`https://inrl-web-fkns.onrender.com/plugins/save?p=${p}&id=${n}`);
+      a.innerHTML = 'okkk';
+}
+    </script>
   </body>
 </html>`
 }
