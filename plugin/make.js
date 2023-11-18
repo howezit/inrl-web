130088d739b827221dc8656a77f9e0a9d5cdc90e
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-18 18:52:04 +0530

    Update make.js

--- plugin/make.js
+++ plugin/make.js
@@ -15,7 +15,7 @@ return `<html>
 const a = document.getElementById("like");
 async function u(p, n) {
       alert(p+",,, "+n);
-      await axios('https://inrl-web-fkns.onrender.com/plugins/save?p='+p+'&id='+n);
+      await axios(https://inrl-web-fkns.onrender.com/plugins/save?p=${p}&id=${n});
       a.innerHTML = 'okkk';
 }
     </script>
