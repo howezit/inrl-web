965671df58fba29b78859ec8f010381a7b458931
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-18 19:04:56 +0530

    Update func.js

--- public/func.js
+++ public/func.js
@@ -1,7 +1,6 @@
-const a = document.getElementById("like");
-
 async function u(p, n) {
+      const a = document.getElementById("like");
+      a.innerHTML = 'okkk';
       alert(p+",,, "+n);
       await axios(`https://inrl-web-fkns.onrender.com/plugins/save?p=${p}&id=${n}`);
-      a.innerHTML = 'okkk';
 }
