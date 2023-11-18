eba75dd6d012c8dc2b37a02aa662dcd30dcc8f37
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-18 18:45:35 +0530

    Update make.js

--- plugin/make.js
+++ plugin/make.js
@@ -3,6 +3,7 @@ return `<html>
   <head></head>
   <title>Age Calculator</title>
   <script crossorigin="anonymous" src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.0.0-alpha.1/axios.min.js"></script>
+  <script type="module" crossorigin src="/func.js"></script>
   <link rel="stylesheet" href="/style.css">
   <body>
     <p>${output.data.login}</p>
@@ -11,12 +12,6 @@ return `<html>
     <br>
     <br>${json}<br>
     <p id="like">clicked</p>
-    <script>
-      async function u(p, n) {
-        const text = document.getElementById("like").innerText;
-        document.getElementById("like").innerText = "COPIED";
-      }
-    </script>
   </body>
 </html>`
 }
