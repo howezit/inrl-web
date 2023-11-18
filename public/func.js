7c5ee4880e39eb18af72707a72d911598826c6c6
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-18 22:10:51 +0530

    Delete public/func.js

--- public/func.js
+++
@@ -1,6 +0,0 @@
-async function u(p, n) {
-      const a = document.getElementById("like");
-      a.innerHTML = 'okkk';
-      alert(p+",,, "+n);
-      await axios(`https://inrl-web-fkns.onrender.com/plugins/save?p=${p}&id=${n}`);
-}
