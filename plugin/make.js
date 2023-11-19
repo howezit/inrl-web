a6be6315578ae57b60095b882f81143535dd33bc
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-19 10:40:26 +0530

    Update make.js

--- plugin/make.js
+++ plugin/make.js
@@ -43,7 +43,7 @@ ${json}
     let likeIcon = document.querySelector(`#${boxId} .like-icon`);
     let likeCounter = document.querySelector(`#${boxId} .like-count`);
     const {data} = await axios(`https://inrl-web-fkns.onrender.com/plugins/save?p=${boxId}&id=${name}`);
-    if (data.status.contains('liked')) {
+    if (data.status.iincludes('liked')) {
       likeCounter.innerText = parseInt(likeCounter.innerText) + 1;
     } else {
       likeCounter.innerText = parseInt(likeCounter.innerText) - 1;
