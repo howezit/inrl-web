3059afd784b463cb5ba389319b308f42dcb63c5e
inrl-official <fasweehmon2@gmail.com>
2023-11-20 00:29:06 +0530

    Update make.js

--- plugin/make.js
+++ plugin/make.js
@@ -4,6 +4,7 @@ return `<html>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>EXTERNAL pluginS</title>
     <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script crossorigin="anonymous" src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.0.0-alpha.1/axios.min.js"></script>
   <link rel="stylesheet" href="/style.css">
 <script>`+
