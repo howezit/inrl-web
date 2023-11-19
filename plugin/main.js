abf121f2efd402fc0c0cbd514da99fb2e6e1c7ac
inrl-official <fasweehmon2@gmail.com>
2023-11-20 00:02:30 +0530

    Update main.js

--- plugin/main.js
+++ plugin/main.js
@@ -43,7 +43,7 @@ router.get('/ack', async (req, res, next) => {
         const output = await octokit.request('GET /user', {});
         const outp = (await axios(`${url}/plugins/get`)).data;
         const json = outp.data.map(a => `<div class="text-box" id="${a.cmd}">
-  <i class="bx bx-heart like-icon" onclick="ulike('${a.cmd}','${output.data.login}')"></i>
+  <i class="bx bx-heart like-icon" style="color: ${a.like.includes(output.data.login)?'red':'white'}" onclick="ulike('${a.cmd}','${output.data.login}')"></i>
   <div class="like-count">${a.like.length}</div>
   <div class="text-maker">${a.cmd}<span class="by"> by </span><span class="dev" onclick="window.location.href='${a.creator.u}';">${a.creator.n}</span></div>
   <div id="description">${a.desc}</div>
