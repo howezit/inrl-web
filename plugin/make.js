bbaa52df08fdb55da5f5a810ca9f1f50892cc304
inrl-md <109638623+inrl-md@users.noreply.github.com>
2023-11-19 13:23:52 +0530

    Update make.js

--- plugin/make.js
+++ plugin/make.js
@@ -1,9 +1,11 @@
 function genHtml(output,json){
 return `<html>
+<head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Age Calculator</title>
   <script crossorigin="anonymous" src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.0.0-alpha.1/axios.min.js"></script>
   <link rel="stylesheet" href="/style.css">
+  </head>
   <body>
     <p>${output.data.login}</p>
     <br>
