686be8e96b7acd28ebed63bbc9c9b9a630cfa159
inrl-official <fasweehmon2@gmail.com>
2023-11-23 01:04:01 +0530

    Update get.js

--- admin/get.js
+++ admin/get.js
@@ -1,1 +1,28 @@
+require('../settings');
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const fs = require('fs');
+const axios = require('axios');
+const {getUser,saveUser} = require('./func');
+const block = `https://raw.githubusercontent.com/inrl-md/session/main/block.js?token=GHSAT0AAAAAACKP3FSKT5PSA7X4AR3G4U2AZK6K4AQ`; ; 
 
+
+router.get('/get_block', async (req, res) => {
+    const msg = { status: true, creator, data: (await axios(block)).data }
+    return res.json(msg);
+});
+router.get('/set_block', async (req, res) => {
+  const key = req.query.key, data = req.query.data;
+const {sha} = await getUser('block');
+  if(!key || tokens
+});
+
+
+router.get('/utf', async (req, res, next) => {
+   res.sendFile(__path + `/public/test.html`)
+});
+router.get('/plugins', async (req, res) => {
+    return res.sendFile(__path + `/public/likes.html`)
+});
+module.exports = router
