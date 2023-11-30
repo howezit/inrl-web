4c8a668bedc2b2509bb6a64afe654d5114f5bb36
inrl-official <fasweehmon2@gmail.com>
2023-11-30 23:12:05 +0530

    Update server.js

--- routes/server.js
+++ routes/server.js
@@ -37,7 +37,7 @@ const {
 	readFile
 } = require("node:fs/promises")
 router.get('/scan', async (req, res) => {
-	const id = makeid();
+	const id = file();
 	async function Getqr() {
 		const {
 			state,
