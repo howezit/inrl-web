7b7e7fbd3bde2ebc831ab65d1a9a53c336f422e5
inrl-official <fasweehmon2@gmail.com>
2023-11-30 16:26:39 +0530

    Update encrypt.js

--- encrypt.js
+++ encrypt.js
@@ -23,4 +23,25 @@ b.map((m)=>{
 })
   return d;
 }
-module.exports = { makeid, encrypt };
+
+function decrypt(session){
+    session = session.replace(/inrl~/,'');
+    let b = session.split("")
+    let c = "",l="",d="",t;
+    b.map((m)=>{
+        if(c.length<5){
+            c += m;
+        } else {
+            l = session.replace(c,'');
+        }
+        let q = l.split("");
+        q.map((r)=>{
+            if(d.length < 4 ){
+                d += r; 
+            }
+        })
+    })
+    t = c + session.replace(c,'').replace(d,'');
+    return t;
+    }
+module.exports = { makeid, encrypt, decrypt };
