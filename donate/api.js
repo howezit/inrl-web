c0f9cdd6fa4c66feaa0e206fdcaa9e8ad226d66f
inrl-official <fasweehmon2@gmail.com>
2023-12-13 21:42:39 +0530

    Update api.js

--- donate/api.js
+++ donate/api.js
@@ -6,6 +6,6 @@ router.get('/generate_upi_qr', async (req, res) => {
 	const mony = req.query.id;
 if(!mony) return error400(res);
 	const url = generateURL({amount: mony});
-	return await res.end(
+	return await res.end(await genrateQR(url));
 });
 module.exports = router
