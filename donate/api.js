61c47f1c787cd0df4c099000b2ad97214c236ad5
inrl-official <fasweehmon2@gmail.com>
2023-12-13 21:40:13 +0530

    Update api.js

--- donate/api.js
+++ donate/api.js
@@ -1,7 +1,9 @@
+require('../settings');
+const {} = require('./func');
 const express = require('express');
 const router = express.Router();
 router.get('/generate_upi_qr', async (req, res) => {
 	const mony = req.query.id;
-if(!mony) return res
+if(!mony) return error400(res);
 });
 module.exports = router
