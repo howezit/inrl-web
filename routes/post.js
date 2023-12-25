983df8c7c4072770a0d006db7ac2a2cf3c2be522
inrl-official <fasweehmon2@gmail.com>
2023-12-25 22:35:31 +0530

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -3,8 +3,8 @@ let router = express.Router();
 const fs = require('fs');
 const {ocrSpace} = require('../lib');
 
-router.post('/upload', function(req, res) {
-  res.send(JSON.stringify(req.files)); // the uploaded file object
+router.post('/test', function(req, res) {
+  res.send(require('util').format(req)); // the uploaded file object
 });
 
 router.post('/ocr', async (req, res, next) => {
