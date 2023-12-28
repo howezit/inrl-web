d04521dd5258bc7cd207e2e8bcde730c962d80a7
inrl-official <fasweehmon2@gmail.com>
2023-12-29 02:25:51 +0530

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -3,16 +3,19 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const {write,htmlColor} = require('../lib');
-
+const x_possible = ['HORIZONTAL_ALIGN_CENTER', 'HORIZONTAL_ALIGN_LEFT', 'HORIZONTAL_ALIGN_RIGHT'];
+const y_possible = ['VERTICAL_ALIGN_BOTTOM', 'VERTICAL_ALIGN_MIDDLE', 'VERTICAL_ALIGN_TOP'];
 router.post('/writer', function(req, res) {
   const buff = req.files.file.data;
   const size = req.body.size ? `FONT_SANS_${req.body.size}_BLACK` : null;
   const text = req.body.text;
-  const x = req.body.x;
-  const y = req.body.y;
+  const x = req.body.x ? `HORIZONTAL_ALIGN_${req.body.x.toUpperCase()}`: null;
+  const y = req.body.y ? `VERTICAL_ALIGN_${req.body.x.toUpperCase()}`: null;
   const color = req.body.color;
   if(!buff || !size || !text || !x || !y || !color) return error503(res);
-  if(!htmlColor(color.toLowerCase()) return res.json({status: false, creator,'inavlid color provided'});
+  if(!x_possible.includes(x)) return res.json({status: false, creator,message: 'x position must be center, left, right'});
+  if(!y_possible.includes(y)) return res.json({status: false, creator,message: 'y position must be bottom, middle, top'});
+  if(!htmlColor(color.toLowerCase()) return res.json({status: false, creator,message: 'inavlid color provided'});
   const file = await write(buff, {size, text, x, y, color: color.toLowerCase()});
   return await res.end(file);
 });
