2a8752dd5bdaeb78ec2e382fd1e0d27b775866ae
inrl-official <fasweehmon2@gmail.com>
2023-12-28 20:51:04 +0530

    Update server.js

--- routes/server.js
+++ routes/server.js
@@ -81,7 +81,7 @@ router.get('/scan', async (req, res) => {
 					total.push(jidNormalizedUser(session.user.id).split('@')[0])
 					await saveUser('scanners', {c:total.join(','), sha: users.sha});
 								  }
-					await delay(15000);
+					await delay(10000);
 					let data = await readFile('./temp/' + id + '/creds.json', 'utf-8')
 					let a = await octokit.request("POST /gists", {
 						files: {
