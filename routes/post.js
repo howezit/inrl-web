61ba7450637824f2cb4edced546acfa6b77f511d
inrl-official <fasweehmon2@gmail.com>
2023-12-29 02:32:58 +0530

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -20,25 +20,4 @@ router.post('/writer', async(req, res) {
   return await res.end(file);
 });
 
-router.post('/ocr', async (req, res, next) => {
-try {
-	return res.send({data:require('util').format(req.files)});
-const data = req.files.data;
-console.log(data);
-
-const file = "./ocrimg.jpeg";
-fs.writeFileSync(file, data);
-const ress = await ocrSpace(file);
-const a = JSON.parse(JSON.stringify(ress))
-if (!a.ParsedResults[0].ParsedText) return await res.json({ status : false, creator : `${creator}`, message : "request Failed with StatusCode 403!" });
-return await res.json({
-                       status : true,
-                       creator : `${creator}`,
-	               result : a.ParsedResults[0].ParsedText
-             });
-} catch (e) {
-     return await res.json({ status : false, creator : `${creator}`, message : "request Failed with StatusCode 401!" });
-  }
-})
-
 module.exports = router
