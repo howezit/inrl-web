7af2d1b5635fea18a7436aadb2dd7c1897581e79
inrl-official <fasweehmon2@gmail.com>
2024-01-03 15:07:40 +0530

    Update get.js

--- admin/get.js
+++ admin/get.js
@@ -5,7 +5,6 @@ const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
 const {getUser,saveUser,decrypt} = require('../lib');
-const gist = 'https://gist.github.com/inrl-official/cd891f301b5786d3dcbe921fbbcc8c4e/raw';
 
 router.get('/ads', async (req, res) => {
     const key = req.query.key, type = req.query.type, wa = req.query.wa, msg = req.query.msg, git = req.query.git;
@@ -27,8 +26,7 @@ router.get('/ads', async (req, res) => {
 router.get('/get_update', async (req, res) => {
     const key = req.query.key
     if(!key || !tokens.includes(key)) return error400(res);
-    const data = await axios(gist);
-    const msg = { status: true, creator, data: data.data }
+    const msg = { status: true, creator, data: update }
     return res.json(msg);
 });
 router.get('/session', async (req, res) => {
@@ -59,47 +57,23 @@ router.get('/get_block', async (req, res) => {
     const key = req.query.key
     if(!key || !tokens.includes(key)) return error400(res);
     try {
-    const {content} = await getUser('block');
-    const msg = { status: true, creator, data: content.split(',') }
+    const msg = { status: true, creator, data: block }
     return res.json(msg);
     } catch (e) {
         console.log(e);
         return error503(res);
     }
-});
-router.get('/set_block', async (req, res) => {
-  const key = req.query.key, data = req.query.data;
-    if(!key || !tokens.includes(key)) return error400(res);
-    const {sha} = await getUser('block');
-    await saveUser('block', {c:data, sha});
-        return res.json({status:true});
 });
 
 router.get('/get_start_msg', async (req, res) => {
     const key = req.query.key
     if(!key || !tokens.includes(key)) return error400(res);
     try {
-    const {content} = await getUser('start');
-    const msg = { status: true, creator, data: content }
+    const msg = { status: true, creator, data: start }
     return res.json(msg);
     } catch (e) {
         console.log(e);
         return error503(res);
     }
-});
-router.get('/set_start_msg', async (req, res) => {
-  const key = req.query.key, data = req.query.data;
-  if(!key || !tokens.includes(key)) return error400(res);
-  const {sha} = await getUser('start');
-  await saveUser('start', {c:data, sha});
-  return res.json({status:true});
-});
-
-
-router.get('/block', async (req, res, next) => {
-   res.sendFile(__path + `/admin/block.html`)
-});
-router.get('/start', async (req, res) => {
-    return res.sendFile(__path + `/admin/start.html`)
 });
 module.exports = router
