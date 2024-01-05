0890504e23d613aefd457ec46e000900fe307d3e
inrl-official <fasweehmon2@gmail.com>
2024-01-05 21:01:22 +0530

    Create stalk.js

---
+++ routes/stalk.js
@@ -0,0 +1,67 @@
+require('../settings');
+const express = require('express');
+const router = express.Router();
+const {
+	igstalk,
+	ytChannel,
+	gitUser,
+	checkkey,
+	getLimit
+} = require('../lib');
+router.get('/ig', async (req, res) => {
+	try {
+		const id = req.query.name;
+		const apikey = req.query.apikey;
+		if (!apikey) return errorMsg(res, 'no apikey provided');
+		if (!keys.includes(apikey)) return errorMsg(res, 'apikey not registered');
+		if (!await checkkey(apikey)) return errorMsg(res, 'apikey limit over');
+		await addLimit(apikey);
+		if (!id) return errorMsg(res, 'missing parameter name');
+		const data = await igstalk(id);
+		return res.json({
+			status: true
+			creator: `${creator}`,
+			result: data
+		});
+	} catch (e) {
+		return error200(res);
+	}
+})
+router.get('/ytchannel', async (req, res) => {
+	try {
+		const id = req.query.name;
+		const apikey = req.query.apikey;
+		if (!apikey) return errorMsg(res, 'no apikey provided');
+		if (!keys.includes(apikey)) return errorMsg(res, 'apikey not registered');
+		if (!await checkkey(apikey)) return errorMsg(res, 'apikey limit over');
+		await addLimit(apikey);
+		if (!id) return errorMsg(res, 'missing parameter name');
+		const data = await igstalk(id);
+
+		return res.json({
+			creator: `${creator}`,
+			result: await ytChannel(id.trim());
+		})
+	} catch (e) {
+		return error200(res);
+	}
+})
+router.get('/github', async (req, res, next) => {
+	try {
+		const id = req.query.user;
+		const apikey = req.query.apikey;
+		if (!apikey) return errorMsg(res, 'no apikey provided');
+		if (!keys.includes(apikey)) return errorMsg(res, 'apikey not registered');
+		if (!await checkkey(apikey)) return errorMsg(res, 'apikey limit over');
+		await addLimit(apikey);
+		if (!id) return errorMsg(res, 'missing parameter user');
+		res.json({
+			status: true,
+			creator: `${creator}`,
+			result: await gitUser(id.trim());
+		})
+	} catch (e) {
+		return error200(res);
+	}
+})
+module.exports = router
