72d1597325d5f074b0da81063d72b54155bc4df3
inrl-official <fasweehmon2@gmail.com>
2024-01-05 08:58:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,61 +4,65 @@ const app = express();
 let path = require('path')
 const bodyParser = require("body-parser");
 const fs = require('fs');
-const {db} = require('./db');
+const {
+	db
+} = require('./db');
 
 const PORT = process.env.PORT || 8000;
 const main = require('./routes/main'),
-    api = require('./routes/api'),
-    server = require('./routes/server'),
-    code = require('./routes/pair'),
-    info = require('./routes/info/info'),
-    post = require('./routes/post'),
-    textpro = require('./routes/textpro'),
-    gfx = require('./routes/gfx'),
-    maker = require('./routes/maker'),
-    vars = require('./routes/info/var'),
-    plugin = require('./plugin/main'),
-    admin = require('./admin/get'),
-    donate = require('./donate/api');
+	api = require('./routes/api'),
+	server = require('./routes/server'),
+	code = require('./routes/pair'),
+	info = require('./routes/info/info'),
+	post = require('./routes/post'),
+	textpro = require('./routes/textpro'),
+	gfx = require('./routes/gfx'),
+	maker = require('./routes/maker'),
+	vars = require('./routes/info/var'),
+	plugin = require('./plugin/main'),
+	admin = require('./admin/get'),
+	donate = require('./donate/api');
 require('events').EventEmitter.defaultMaxListeners = 500;
 const fileUpload = require('express-fileupload');
 async function start() {
-await db.sync();
-app.use(fileUpload());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use('/', main)
-app.set("trust proxy", true);
-app.set("json spaces", 2);
-app.use('/api', api)
-app.use('/api/post', post);
-app.use('/api/textpro', textpro);
-app.use('/api/gfx', gfx);
-app.use('/api/maker', maker);
-app.use('/info/bot/var', vars)
-app.use('/server', server);
-app.use('/pair', code);
-app.use('/info', info)
-app.use('/plugins', plugin);
-app.use('/admin', admin);
-app.use('/donate', donate);
+	await db.sync();
+	app.use(fileUpload());
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}));
+	app.use(express.static("public"));
+	app.use('/', main)
+	app.set("trust proxy", true);
+	app.set("json spaces", 2);
+	app.use('/api', api)
+	app.use('/api/post', post);
+	app.use('/api/textpro', textpro);
+	app.use('/api/gfx', gfx);
+	app.use('/api/maker', maker);
+	app.use('/info/bot/var', vars)
+	app.use('/server', server);
+	app.use('/pair', code);
+	app.use('/info', info)
+	app.use('/plugins', plugin);
+	app.use('/admin', admin);
+	app.use('/donate', donate);
 
 
-app.use(async(req, res, next) => {
-	if(fs.existsSync('.'+req.path)) {
-		const file = fs.readFileSync('.'+req.path);
-		await res.end(file);
-		return fs.unlinkSync('.'+req.path);
-	}
-	res.status(200).json({
-        status: false,
-        message: "Connection Closed"
-    })
-})
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:` + PORT);
-});
+	app.use(async (req, res, next) => {
+		if (fs.existsSync('.' + req.path)) {
+			const file = fs.readFileSync('.' + req.path);
+			await res.end(file);
+			return fs.unlinkSync('.' + req.path);
+		}
+		res.status(200).json({
+			status: false,
+			message: "Connection Closed"
+		})
+	})
+	app.listen(PORT, () => {
+		console.log(`Server running on http://localhost:` + PORT);
+	});
 }
 start()
 module.exports = app
