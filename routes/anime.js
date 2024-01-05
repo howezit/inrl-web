ffa3b0bdf28b26dadec46705b592070e15043e25
inrl-official <fasweehmon2@gmail.com>
2024-01-06 01:06:05 +0530

    Create anime.js

---
+++ routes/anime.js
@@ -0,0 +1,91 @@
+const {
+	husbu,
+	loli,
+	neko,
+	shota,
+	wifu,
+	checkkey,
+	addLimit
+} = require('../lib');
+router.get('/husbu', async (req, res, next) => {
+	try {
+		const apikey = req.query.apikey;
+		if (!apikey) return errorMsg(res, 'no apikey provided');
+		if (!keys.includes(apikey)) return errorMsg(res, 'apikey not registered');
+		if (!await checkkey(apikey)) return errorMsg(res, 'apikey limit over');
+		await addLimit(apikey);
+		res.json({
+			status: true,
+			creator,
+			result: husbu()
+		})
+	} catch (e) {
+		return error200(res);
+	}
+})
+router.get('/loli', async (req, res, next) => {
+	try {
+		const apikey = req.query.apikey;
+		if (!apikey) return errorMsg(res, 'no apikey provided');
+		if (!keys.includes(apikey)) return errorMsg(res, 'apikey not registered');
+		if (!await checkkey(apikey)) return errorMsg(res, 'apikey limit over');
+		await addLimit(apikey);
+		res.json({
+			status: true,
+			creator,
+			result: loli()
+		})
+	} catch (e) {
+		return error200(res);
+	}
+})
+router.get('/neko', async (req, res, next) => {
+	try {
+		const apikey = req.query.apikey;
+		if (!apikey) return errorMsg(res, 'no apikey provided');
+		if (!keys.includes(apikey)) return errorMsg(res, 'apikey not registered');
+		if (!await checkkey(apikey)) return errorMsg(res, 'apikey limit over');
+		await addLimit(apikey);
+		res.json({
+			status: true,
+			creator,
+			result: neko()
+		})
+	} catch (e) {
+		return error200(res);
+	}
+})
+router.get('/shota', async (req, res, next) => {
+	try {
+		const apikey = req.query.apikey;
+		if (!apikey) return errorMsg(res, 'no apikey provided');
+		if (!keys.includes(apikey)) return errorMsg(res, 'apikey not registered');
+		if (!await checkkey(apikey)) return errorMsg(res, 'apikey limit over');
+		await addLimit(apikey);
+		res.json({
+			status: true,
+			creator,
+			result: shota()
+		})
+	} catch (e) {
+		return error200(res);
+	}
+})
+router.get('/wifu', async (req, res, next) => {
+	try {
+		const apikey = req.query.apikey;
+		if (!apikey) return errorMsg(res, 'no apikey provided');
+		if (!keys.includes(apikey)) return errorMsg(res, 'apikey not registered');
+		if (!await checkkey(apikey)) return errorMsg(res, 'apikey limit over');
+		await addLimit(apikey);
+		res.json({
+			status: true,
+			creator,
+			result: wifu()
+		})
+	} catch (e) {
+		return error200(res);
+	}
+})
+
+module.exports = router
