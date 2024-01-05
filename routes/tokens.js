374738a8962fe1091209abd75fa1b0ed56ed7bb9
inrl-official <fasweehmon2@gmail.com>
2024-01-05 11:34:05 +0530

    Update tokens.js

--- routes/tokens.js
+++ routes/tokens.js
@@ -1,3 +1,21 @@
 require('../settings');
 const {apikey,addLimit,checkkey,getLimit} = require('../lib');
+const express = require('express');
+const router = express.Router();
 
+router.get('/limit', async (req, res) => {
+    let id = req.query.apikey;
+    if (!id) return res.json({
+        status: false,
+        creator: `${creator}`,
+        message: 'no key found'
+    })
+    const {status, message, limit} = await getLimit(id);
+    return res.json({
+        creator: `${creator}`,
+        result: data
+    })
+})
+
+
+module.exports = router
