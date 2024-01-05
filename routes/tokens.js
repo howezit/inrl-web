0292729fffaafcbe9ada73bd2f836cf73fc692b7
inrl-official <fasweehmon2@gmail.com>
2024-01-05 11:40:02 +0530

    Update tokens.js

--- routes/tokens.js
+++ routes/tokens.js
@@ -10,12 +10,35 @@ router.get('/limit', async (req, res) => {
         creator: `${creator}`,
         message: 'no key found'
     })
+    if(!checkkey(key)) return res.json({
+        status: false,
+        creator: `${creator}`,
+        message: 'not registered!'
+    })
     const {status, message, limit} = await getLimit(id);
     return res.json({
+        status,
         creator: `${creator}`,
-        result: data
+        result: limit
     })
 })
 
-
+router.get('/check', async (req, res) => {
+    let id = req.query.apikey;
+    if (!id) return res.json({
+        status: false,
+        creator: `${creator}`,
+        message: 'no key found'
+    })
+    if(!checkkey(key)) return res.json({
+        status: false,
+        creator: `${creator}`,
+        message: 'not registered!'
+    })
+    return res.json({
+        status: true,
+        creator: `${creator}`
+        message: 'active'
+    });
+})
 module.exports = router
