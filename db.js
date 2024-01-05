20d932b4ed7dc0577d407f8ee9b5c6a6fd8535f6
inrl-official <fasweehmon2@gmail.com>
2024-01-05 08:03:35 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,8 @@
+const { Sequelize } = require('sequelize');
+
+const db = new Sequelize({
+          dialect: 'sqlite',
+          storage: 'db.db,
+          logging: false,
+        })
+  module.exports = {db};
