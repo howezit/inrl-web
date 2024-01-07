a473081d76fd58173843d10e90a8b139a725dbf4
inrl-official <fasweehmon2@gmail.com>
2024-01-07 10:15:39 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,62 +3,12 @@ __path = process.cwd()
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const {io} = require('../index');
-let localStrategy = require('passport-local').Strategy;
-const passport = require('passport');
-
 const axios = require('axios');
 const cheerio = require('cheerio');
-const {
-	getLogin,
-	saveLogin
-} = require('../lib');
-let router = express.Router()
-router.get('/csrf', async (req, res, next) => {
-	return res.send(req.csrfToken());
-})
 
-router.get('/sign', async (req, res, next) => {
+router.get('/signup', async (req, res, next) => {
 	res.sendFile(__path + '/view/loginapi.html')
-	io.emit('csrf', req.csrfToken());
 })
-
-passport.use(new localStrategy({
-	usernameField: 'key'
-}, async (key, password, done) => {
-	const exist = await getLogin(key);
-	if (!exist) return done(null, false);
-	return done(null, exist);
-}));
-
-passport.serializeUser(function(user, done) {
-	done(null, user.id);
-});
-
-passport.deserializeUser(async(id, done) => {
-	user.findById(id, function(err, user) {
-		done(err, user);
-	});
-});
-
-router.get('/events', async (req, res, next) => {
-	const trylogin = req.query.id;
-	const trysaved = req.query.key;
-	if (trylogin) {
-		await saveLogin(trylogin);
-		res.redirect('/api/events?key=events');
-	} else if (trysaved) {
-		req.body.key = trysaved;
-		passport.authenticate('local', {
-			failureRedirect: '/login',
-			successRedirect: '/docs',
-			failureFlash: true,
-		})(req, res, next);
-	} else {
-		res.redirect('/api/dashboard');
-	}
-})
-
 
 router.get('/phone', async (req, res, next) => {
     let id = req.query.number;
