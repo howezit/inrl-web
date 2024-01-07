e119ae3e7d34736b7393d4035ada117d02eb6e15
inrl-official <fasweehmon2@gmail.com>
2024-01-07 13:51:02 +0530

    Update tokens.js

--- routes/tokens.js
+++ routes/tokens.js
@@ -1,44 +1,19 @@
 require('../settings');
-const {checkkey,getLimit} = require('../lib');
+const {getLimit} = require('../lib');
 const express = require('express');
 const router = express.Router();
 
 router.get('/limit', async (req, res) => {
     let id = req.query.apikey;
-    if (!id) return res.json({
-        status: false,
-        creator: `${creator}`,
-        message: 'no key found'
-    })
-    if(!checkkey(id)) return res.json({
-        status: false,
-        creator: `${creator}`,
-        message: 'not registered!'
-    })
+    if (!id) return errorMsg(res, 'no apikey provided');
     const {status, message, limit} = await getLimit(id);
     return res.json({
         status,
+        message,
         creator: `${creator}`,
         result: limit
     })
 })
 
-router.get('/check', async (req, res) => {
-    let id = req.query.apikey;
-    if (!id) return res.json({
-        status: false,
-        creator: `${creator}`,
-        message: 'no key found'
-    })
-    if(!await checkkey(id)) return res.json({
-        status: false,
-        creator: `${creator}`,
-        message: 'not registered!'
-    })
-    return res.json({
-        status: true,
-        creator: `${creator}`,
-        message: 'active'
-    });
-})
+
 module.exports = router
