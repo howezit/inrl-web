4850aee2f3174b4b97401c88bea602dc5ea50060
inrl-official <fasweehmon2@gmail.com>
2024-01-14 07:09:13 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize({
-          dialect: 'sqlite',
-          storage: './db.db',
-          logging: false,
-        })
+const db = new Sequelize(
+  "postgres://bot:5yqkwMZvrBWX4CKyuelGlaStyZigPb2U@dpg-cmhjdcun7f5s739tahh0-a.oregon-postgres.render.com/inrl_9jbg",
+  {
+    dialect: "postgres",
+    ssl: true,
+    protocol: "postgres",
+    dialectOptions: {
+      native: true,
+      ssl: { require: true, rejectUnauthorized: false },
+    },
+    logging: false,
+  },
+);
   module.exports = {db};
