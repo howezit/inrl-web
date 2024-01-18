ed3de012e97602303cf8cdbeec9dda9bfa5f6841
inrl-official <fasweehmon2@gmail.com>
2024-01-18 07:46:40 +0530

    Update info.js

--- routes/info/info.js
+++ routes/info/info.js
@@ -3,10 +3,10 @@ let router = express.Router();
 const {getBuffer,device,generateRandomString} = require('../../lib');
 const urls = ["https://i.imgur.com/qyvmAzS.jpeg"];
 const wagrp = "https://chat.whatsapp.com/F6VWuK677vB1kxXbV8m5II";
-const koyeb = "https://app.koyeb.com/apps/deploy?type=docker&image=quay.io/inrlwabot/inrl-bot:latest&name=inrl-md&ports=3000;http;/&env[KOYEB_API_KEY]=&env[SESSION_ID]=your_session_here&env[PORT]=3000&env[MONGO_URL]=";
+const koyeb = "https://app.koyeb.com/apps/deploy?type=docker&image=quay.io/inrlwabot/inrl-bot:latest&name=inrl-md&ports=3000;http;/&env[KOYEB_API_KEY]=&env[SESSION_ID]=your_session_here&env[PORT]=3000&env[DATABASE_URL]=";
 const  parsePhoneNumber  = require("awesome-phonenumber");
 const axios = require("axios");
-const railway = "https://railway.app/template/yfpKNM?referralCode=ERZ4-2";
+const railway = "https://github.com/inrl-official/inrl-bot-md";
 
 router.get('/bot/status.jpeg', async (req, res, next) => {
 let url = urls[Math.floor(Math.random() * urls.length)]
