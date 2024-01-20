36b874859985e91e02e0c7de6960cd23ad431973
inrl-official <fasweehmon2@gmail.com>
2024-01-20 07:16:34 +0530

    Create ai.js

---
+++ routes/ai.js
@@ -0,0 +1,52 @@
+require('../settings');
+const express = require('express');
+const router = express.Router();
+const {
+	gpt,
+	addLimit,
+	gimini
+} = require('../lib');
+
+router.get('/chatgpt', async (req, res) => {
+	try {
+		const id = req.query.text;
+		const apikey = req.query.apikey;
+		if (!apikey) return errorMsg(res, 'no apikey provided');
+		const limits = await addLimit(apikey);
+		if (!limits.status) return errorMsg(res, limits.message); 
+		if (!id) return errorMsg(res, 'missing parameter text');
+		let res_msg = await gpt(id);
+		res_msg = res_msg.reply;
+		return await res.json({
+			status: true,
+			creator: `${creator}`,
+			result: res_msg
+		});
+	} catch (e) {
+		console.log(e);
+		return error200(res);
+	}
+});
+router.get('/gimini', async (req, res) => {
+	try {
+		const id = req.query.text;
+		const apikey = req.query.apikey;
+		if (!apikey) return errorMsg(res, 'no apikey provided');
+		const limits = await addLimit(apikey);
+		if (!limits.status) return errorMsg(res, limits.message); 
+		if (!id) return errorMsg(res, 'missing parameter text');
+		let res_msg = await gimini(id);
+		res_msg = res_msg.reply;
+		return await res.json({
+			status: true,
+			creator: `${creator}`,
+			result: res_msg
+		});
+	} catch (e) {
+		console.log(e);
+		return error200(res);
+	}
+});
+
+
+module.exports = router
