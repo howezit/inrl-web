1606c6f8711fc27bcfbdb5195d8f6a112e58a10d
inrl-official <fasweehmon2@gmail.com>
2024-01-20 07:15:11 +0530

    Update tools.js

--- routes/tools.js
+++ routes/tools.js
@@ -70,26 +70,6 @@ router.post('/fancy', async (req, res, next) => {
 		return error200(res);
 	}
 });
-router.get('/chatgpt', async (req, res) => {
-	try {
-		const id = req.query.text;
-		const apikey = req.query.apikey;
-		if (!apikey) return errorMsg(res, 'no apikey provided');
-		const limits = await addLimit(apikey);
-		if (!limits.status) return errorMsg(res, limits.message); 
-		if (!id) return errorMsg(res, 'missing parameter text');
-		let res_msg = await gpt6(id);
-		res_msg = res_msg.reply;
-		return await res.json({
-			status: true,
-			creator: `${creator}`,
-			result: res_msg
-		});
-	} catch (e) {
-		console.log(e);
-		return error200(res);
-	}
-});
 
 router.get('/qrcode', async (req, res) => {
 	try {
