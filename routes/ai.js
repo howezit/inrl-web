fd503f99701856f741f4a522bd3423116e61c9e3
inrl-official <fasweehmon2@gmail.com>
2024-01-20 07:59:28 +0530

    Update ai.js

--- routes/ai.js
+++ routes/ai.js
@@ -35,12 +35,10 @@ router.get('/gemini', async (req, res) => {
 		const limits = await addLimit(apikey);
 		if (!limits.status) return errorMsg(res, limits.message); 
 		if (!id) return errorMsg(res, 'missing parameter text');
-		let res_msg = await gemini(id);
-		res_msg = res_msg.reply;
 		return await res.json({
 			status: true,
 			creator: `${creator}`,
-			result: res_msg
+			result: await gemini(id)
 		});
 	} catch (e) {
 		console.log(e);
