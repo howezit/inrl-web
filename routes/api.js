36e3bc2b29e9f3865b8faba6b900032b065ec3d8
inrl-md <109638623+inrl-md@users.noreply.github.com>
2024-03-24 02:56:26 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const router = express.Router()
+const {mailTransporter} = require('../lib');
+
 
 router.get('/signup', async (req, res, next) => {
 	res.sendFile(__path + '/view/loginapi.html')
@@ -13,6 +15,31 @@ router.get('/signup', async (req, res, next) => {
 router.get('/dashboard', async (req, res, next) => {
 res.sendFile(__path + '/view/apidashboard.html')
 })
+
+router.post('/mail', async (req, res)=>{
+	const {mail, host} = req.body;
+	if(!mail || !host) return error400(res);
+try {
+	    mailTransporter.sendMail({
+      from: 'inrlwabots@gmail.com',
+      to: id,
+      subject: 'VERIFICATION',
+      html: `<html>
+        <body>
+            <h4 align="center">Verification</h4>
+            <p>The 6 digit number given below is your verification code, after copying this you can go to the bot number and use verify cmd to verify it, but you can give a limit to the primimum cmds.</p>
+            <h3 align="center">copy the Below code</h2><br>
+            <a size="30" href="https://${host}/${otp} align="center">Login</font>
+        </body>
+    </html>`});
+} catch (e) {
+	return await res.json({
+            status: false,
+            creator,
+            message: e
+        });
+}
+});
 router.get('/phone', async (req, res, next) => {
     let id = req.query.number;
     if (!id) return await res.json({
