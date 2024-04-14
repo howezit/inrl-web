236b1beddf4ff2d78ef00ae8ca13bb455243cee1
inrl <109638623+INR-L@users.noreply.github.com>
2024-04-14 07:28:13 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,16 +1,10 @@
 const { Sequelize } = require('sequelize');
 
-const db = {} /*new Sequelize(
-  "postgres://bot:5yqkwMZvrBWX4CKyuelGlaStyZigPb2U@dpg-cmhjdcun7f5s739tahh0-a.oregon-postgres.render.com/inrl_9jbg",
-  {
-    dialect: "postgres",
-    ssl: true,
-    protocol: "postgres",
-    dialectOptions: {
-      native: true,
-      ssl: { require: true, rejectUnauthorized: false },
-    },
-    logging: false,
-  },
-);*/
+const db = new Sequelize(
+  { 
+    dialect:'sqlite',
+    storage:'./database.db',
+    logging:false
+  }
+);
   module.exports = {db};
